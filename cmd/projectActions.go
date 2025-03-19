d37888ea90c16d965b1034df27ccfa1fccdd3788
stfmarkov <stf.markov@gmail.com>
2025-03-19 09:13:40 +0200

    add command action. WiP

--- cmd/projectActions.go
+++ cmd/projectActions.go
@@ -85,18 +85,38 @@ func openProject(project string) {
 	fmt.Println("Unsupported OS")
 }
 
+func addCommand(project string) {
+	DirectInput("Adding command for project ( If the commands you entered do not work ... skill issues ): ", func(project string) {
+
+		fmt.Println("Will add command for project", project)
+
+		// err := config.AddProjectDir(project)
+
+		// if err != nil {
+		// 	fmt.Println("Error adding project directory:", err)
+		// 	return
+		// }
+
+		// fmt.Println("Project directory added successfully")
+		// listAllProjects()
+
+	})
+}
+
 func showProjectActions(project string) {
 	fmt.Println("Will show project actions for", project)
 
 	actions := []string{
 		"Navigate to project",
 		"Open project",
+		"Add command",
 		// TODO: Add more actions
 	}
 
 	implementedActions := map[string]func(string){
 		"Navigate to project": navigateToProject,
 		"Open project":        openProject,
+		"Add command":         addCommand,
 	}
 
 	takeAction := func(action string) {
